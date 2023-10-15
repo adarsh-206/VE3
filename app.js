9f46ee1cb05165f9a97da075e63dc72cbf4a28bc
adarsh-206 <adarshdhote8624@gmail.com>
2023-10-15 10:03:07 +0530

    Deployment Done

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const mongoose = require("mongoose");
 const path = require("path");
 const cors = require("cors");
-require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const profileRoutes = require("./routes/profileRoutes");
@@ -11,8 +11,7 @@ const profileRoutes = require("./routes/profileRoutes");
 app.use(express.json());
 app.use(cors());
 
-const mongoUrl = process.env.MONGODB_URL;
-mongoose.connect("mongodb://user:user@ac-9qzpedu-shard-00-00.s1peixs.mongodb.net:27017,ac-9qzpedu-shard-00-01.s1peixs.mongodb.net:27017,ac-9qzpedu-shard-00-02.s1peixs.mongodb.net:27017/?ssl=true&replicaSet=atlas-dpr11b-shard-0&authSource=admin&retryWrites=true&w=majority", err => {
+mongoose.connect(process.env.MONGODB_URL, err => {
   if (err) throw err;
   console.log("Mongodb connected...");
 });
